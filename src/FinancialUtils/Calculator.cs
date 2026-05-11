efb4d72fa42344a8da5fa6687e99b19508ece0ec
yane340 <yanela.340@gmail.com>
2026-05-11 11:17:05 -0600

    Update Calculator.cs
    Agrega suma de enteros

--- src/FinancialUtils/Calculator.cs
+++ src/FinancialUtils/Calculator.cs
@@ -10,6 +10,8 @@ public static class Calculator
     /// </summary>
     public static decimal Add(decimal a, decimal b) => a + b;
 
+     public static  int Add(int a, int b) => a + b;
+
     /// <summary>
     /// Resta b de a.
     /// </summary>
